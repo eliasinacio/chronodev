261e7d82eaafaba00f4bb0e074bd65e4a10bc809
Elias Inácio <0.elias.inacio@gmail.com>
2021-06-17 21:12:58 -0300

    added sass and create a simple timer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import './App.scss';
+import Timer from './Timer';
 
 function App() {
   return (
     <div className="App">
+      <Timer />
       <p>Hello World</p>
     </div>
   );
