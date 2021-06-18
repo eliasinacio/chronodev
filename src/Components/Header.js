9020b8d04a87b112186f0aa9ef8ca9ca23a908af
Elias Inácio <0.elias.inacio@gmail.com>
2021-06-18 15:31:07 -0300

    update header links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,17 +5,17 @@ class Header extends React.Component {
   render () {
     return (
       <header>
-        <div id="home">
-          <a href="localhost:3000">
+        <div id="home">          
+          <a href="https://github.com/eliasinacio/react-pomofocus-clone/" target="_blank"  rel="noreferrer">
             <img src="https://img.icons8.com/pastel-glyph/48/ffffff/square-clock.png" alt="A simple square clock icon"/>
-            <h2>ChronoDev</h2>
+            <h3>ChronoDev</h3>
           </a>
         </div>
 
         <div id="github">
           <a href="https://github.com/eliasinacio/react-pomofocus-clone/" target="_blank"  rel="noreferrer">
             <img src="https://img.icons8.com/fluent-systems-filled/48/ffffff/github.png" alt="Github octocat icon"/>
-            <h4>GitHub</h4>
+            <p>GitHub</p>
           </a>
         </div>
       </header>
