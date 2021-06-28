f0047a679f4164ff13d9baf90a7d85cdd9e68b11
Elias Inacio <0.elias.inacio@gmail.com>
2021-06-28 19:31:48 -0300

    estou ficando louco. :)

--- src/Components/Timer.tsx
+++ src/Components/Timer.tsx
@@ -1,56 +1,55 @@
-import { useState } from 'react'
-import '../styles/timer.scss'
-
-type timerTypes = {
-  min: number;
-  sec: number;
-}
+import { useState } from "react";
 
 export function Timer () {
+  
   const [ selectedTimer, setSelectedTimer ] = useState(1);
   const [ paused , pauseTimer ] = useState(true);
   
-  const [ time ] = useState({ min: 25, sec: 0} as timerTypes);
-
-  let date = 0;
-
-  function handleSetTimeTo (minutes: number) {
-    if (minutes === 25) setSelectedTimer(1)
-    if (minutes === 5) setSelectedTimer(2)
-    if (minutes === 15) setSelectedTimer(3)
-  }
-
-  function handleStartTimer() {
-    paused ? pauseTimer(false) : pauseTimer(true);
-  }
+  const [ min, setMin ] = useState(25);
+  const [ sec, setSec ] = useState(0);
   
+  function handleSetTimeTo (selected: number) {
+    pauseTimer(true);
+
+    setSelectedTimer(selected);
+    
+    if (selected === 1) setMin(25);
+    if (selected === 2) setMin(5);
+    if (selected === 3) setMin(15);
+    
+    setSec(0);
+  }
+
+  const displayMinutes = min > 9 ? min : `0${min}`
+  const displaySeconds = sec > 9 ? sec : `0${sec}`
+
   return (
     <main>
       <div className="container">
         <div className="buttons">
           <button 
             id="pomodoroBtn" className={`btn ${selectedTimer === 1 && 'selected'}`}
-            onClick={ () => handleSetTimeTo(25) }
+            onClick={ () => handleSetTimeTo(1) }
           >
             Pomodoro
           </button>
           <button 
             id="shortBreackBtn" className={`btn ${selectedTimer === 2 && 'selected'}`}
-            onClick={ () => handleSetTimeTo(5) }
+            onClick={ () => handleSetTimeTo(2) }
           >
             Short break
           </button>
           <button 
             id="longBreakBtn" className={`btn ${selectedTimer === 3 && 'selected'}`}
-            onClick={ () => handleSetTimeTo(15) }
+            onClick={ () => handleSetTimeTo(3) }
           >
             Long break
           </button> 
         </div>
 
         <div className="timer">
-          <h3>{time.min < 10 ? `0${time.min}` : time.min}:{time.sec < 10 ? `0${time.sec}` : time.sec}</h3>
-          <button onClick={ handleStartTimer }>{ paused ? 'START' : 'STOP' }</button>
+          <h3>{displayMinutes}:{displaySeconds}</h3>
+          <button onClick={ () => pauseTimer(!paused) }>{ paused ? 'START' : 'STOP' }</button>
         </div>
 
       </div>
